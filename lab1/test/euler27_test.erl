3d4f277dfa4d9c30545788a76a0184acbaaf1a5c
d.lyanguzov <d.lyanguzov@geoscan.ru>
2024-10-22 11:28:10 +0300

    init code formatter, format files

--- lab1/test/euler27_test.erl
+++ lab1/test/euler27_test.erl
@@ -4,7 +4,7 @@
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("stdlib/include/assert.hrl").
 
--define(ANSWER, {-59231,-61,971,71}).
+-define(ANSWER, {-59231, -61, 971, 71}).
 
 euler27_recursion_test() -> ?assertEqual(?ANSWER, euler27_recursion:solution()).
 
